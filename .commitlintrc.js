34b5faaa3e3fc1a7240b0b51ed6961126f6d23a0
Joseph Thomas <joseph@sanctuary.computer>
2021-11-24 08:35:18 -0800

    Release (#57)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,6 +2,7 @@ module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'body-max-line-length': [1, 'always', 100],
+		'footer-max-line-length': [1, 'always', 500],
 		'scope-empty': [2, 'never'],
 		'scope-enum': [
 			2,
